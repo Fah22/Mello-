19cf7d103826643d4211fbee6efaf9da32c33982
Fadziso Gabrielle <fadziso@novodigital.biz>
2021-11-14 14:25:13 +0200

    Added Images

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,46 +1,45 @@
 import React from "react";
-import '../components/Data';
 
 const Gallery = () => {
 
     const data = [
         {
             "id": 1,
-            "image": "./assets/ian-schneider-TamMbr4okv4-unsplash.jpg"
+            "image": "images/ian-schneider-TamMbr4okv4-unsplash.jpg"
         },
     
         {
             "id": 2,
-            "image": "./assets/kelly-sikkema-o2TRWThve_I-unsplash.jpg",
+            "image": "images/kelly-sikkema-o2TRWThve_I-unsplash.jpg",
         },
     
         {
             "id": 3,
-            "image": "./assets/kevin-jarrett-wkbQjSfc6Gw-unsplash.jpg",
+            "image": "images/kevin-jarrett-wkbQjSfc6Gw-unsplash.jpg",
         },
     
         {
             "id": 4,
-            "image": "./assets/oladimeji-odunsi-br7vCg8693o-unsplash.jpg",
+            "image": "images/oladimeji-odunsi-br7vCg8693o-unsplash.jpg",
         },
     
         {
             "id": 5,
-            "image": "./assets/soundtrap-18DcGekC7qE-unsplash.jpg",
+            "image": "images/soundtrap-18DcGekC7qE-unsplash.jpg",
         },
     
         {
             "id": 6,
-            "image": "../assets/vinicius-amnx-amano-DZ0MfTX_9sQ-unsplash.jpg"
+            "image": "images/vinicius-amnx-amano-DZ0MfTX_9sQ-unsplash.jpg"
         }
     ]
 
     return (
         <>
-            {data.map((image, index) => (
-                <div key={index} class="grid">
+            {data.map((item) => (
+                <div key={item.id} class="grid">
                     <div class="g-col-6 g-col-md-4">
-                        <img src={data.image} alt="Gallery Pic" />
+                        <img src={item.image} alt="Gallery Pic" style={{ width: "200px", height: "200px"}}/>
                     </div>
                 </div>
             ))}
